3b0c97f1bbf530107ff33fd2a7cc7433fda40d9c
ViViCouBar <40126535+Vivicoubar@users.noreply.github.com>
2024-06-17 19:29:16 +0200

    Add code for the functions in a separate branch

--- Compile.py
+++ Compile.py
@@ -4,30 +4,63 @@ cpt = 0
 
 op2asm = {"+": "add rax, rbx", "-": "sub rax, rbx"}
 
-def compile(ast):
-    asmString = ""
-    asmString = asmString + "extern printf, atol ;déclaration des fonctions externes\n"
-    asmString = asmString + "global main ; declaration main\n"
-    asmString = asmString + "section .data ; section des données\n"
-    asmString = asmString + "long_format: db '%lld',10, 0 ; format pour les int64_t\n"
-    asmString = asmString + "argc : dq 0 ; copie de argc\n"
-    asmString = asmString + "argv : dq 0 ; copie de argv\n"
-    asmVar, vars = variable_declaration(ast.children[0])
-    asmString = asmString + asmVar
-    asmString = asmString + "section .text ; instructions\n"
-    asmString += "main :"
-    asmString += "push rbp; Set up the stack. Save rbp\n"
-    asmString += "mov [argc], rdi\n"
-    asmString += "mov [argv], rsi\n"
-    asmString += initMainVar(ast.children[0])
-    asmString += compilCommand(ast.children[1])
-    asmString += compilReturn(ast.children[2])
-    asmString += "pop rbp\n"
-    asmString += "xor rax, rax\n"
-    asmString += "ret\n"
-    return asmString
 
-def variable_declaration(ast) :
+def compile_ast(ast):
+    try:
+        asmString = ""
+        asmString = asmString + "extern printf, atol ;déclaration des fonctions externes\n"
+        asmString = asmString + "section .data ; section des données\n"
+        asmString = asmString + "long_format: db '%lld',10, 0 ; format pour les int64_t\n"
+        asmString = asmString + "argc : dq 0 ; copie de argc\n"
+        asmString = asmString + "argv : dq 0 ; copie de argv\n"
+        asmFunc, func = function_declaration(ast.children[0])
+        asmVar, vars = variable_declaration(ast.children[0])
+        asmString = asmString + asmVar
+        asmString = asmString + "section .text ; instructions\n"
+        asmString += "main :"
+        asmString += "push rbp; Set up the stack. Save rbp\n"
+        asmString += "mov [argc], rdi\n"
+        asmString += "mov [argv], rsi\n"
+        asmString += initMainVar(ast.children[0])
+        asmString += compilCommand(ast.children[1])
+        asmString += compilReturn(ast.children[2])
+        asmString += "pop rbp\n"
+        asmString += "xor rax, rax\n"
+        asmString += "ret\n"
+        return asmString
+    except ValueError as e:
+        print("Error was raised:" + str(e))
+
+
+def function_declaration(ast):
+    asmFunc = ""
+    asmVar = ""
+    vars = set()
+    func = set()
+    contains_main = False
+    for child in ast.children:
+        if child.data == "func":
+            if child.children[0].value in func:
+                raise SyntaxError(f"Function {child.children[0].value} already defined")
+            if child.children[0].value == "main":
+                contains_main = True
+            # Function declaration
+            func.add(child.children[0].value)
+            # Declare the variables
+            curVarAsm, curVarSet = variable_declaration(child.children[1])
+
+            asmFunc += f"{child.children[0].value}:\n"
+            asmFunc += "push rbp\n"
+            asmFunc += "mov rbp, rsp\n"
+            asmFunc += compilCommand(child.children[1])
+            asmFunc += compilReturn(child.children[2])
+            asmFunc += "pop rbp\n"
+            asmFunc += "ret\n"
+    if not contains_main:
+        raise SyntaxError("No main function found. Your program needs a main function")
+
+
+def variable_declaration(ast):
     asmVar = ""
     vars = set()
     if ast.data != "liste_vide":
@@ -36,18 +69,20 @@ def variable_declaration(ast) :
             vars.add(child.value)
     return asmVar, vars
 
+
 def initMainVar(ast):
     asmVar = ""
     if ast.data != "liste_vide":
         index = 0
         for child in ast.children:
             asmVar += "mov rbx, [argv]\n"
-            asmVar += f"mov rdi, [rbx + { 8*(index+1)}]\n"
+            asmVar += f"mov rdi, [rbx + {8 * (index + 1)}]\n"
             asmVar += "xor rax, rax\n"
             asmVar += "call atol\n"
-            asmVar += f"mov [{child.value}], rax\n"
+            asmVar += f"mov [{child.value.strip()}], rax\n"
             index += 1
     return asmVar
+
 
 def compilReturn(ast):
     asm = compilExpression(ast)
@@ -56,6 +91,7 @@ def compilReturn(ast):
     asm += "xor rax, rax \n"
     asm += "call printf \n"
     return asm
+
 
 def compilCommand(ast):
     asmVar = ""
@@ -71,17 +107,20 @@ def compilCommand(ast):
         asmVar = compilPrintf(ast)
     return asmVar
 
+
 def compilWhile(ast):
     global cpt
     cpt += 1
     return f""" 
-            loop{cpt} : {compilExpression(ast.children[0])}
+            loop{cpt} :
+                {compilExpression(ast.children[0]).strip()}
                 cmp rax, 0
                 jz fin{cpt}
-                {compilCommand(ast.children[1])}
+                {compilCommand(ast.children[1]).strip()}
                 jmp loop{cpt}
             fin{cpt} :
         """
+
 
 def compilIf(ast):
     global cpt
@@ -94,35 +133,38 @@ def compilIf(ast):
             fin{cpt} :
         """
 
+
 def compilSequence(ast):
     asm = ""
-    for child in ast.children :
-        asm +=compilCommand(child)
+    for child in ast.children:
+        asm += compilCommand(child)
     return asm
+
 
 def compilAsgt(ast):
     asm = compilExpression(ast.children[1])
-    asm += f"mov [{ast.children[0].value}], rax \n"
+    asm += f"mov [{ast.children[0].value.strip()}], rax \n"
     return asm
 
+
 def compilPrintf(ast):
-    asm = compilExpression(ast.children[0])
+    asm = compilExpression(ast.children[0]).strip()
     asm += "mov rsi, rax \n"
     asm += "mov rdi, long_format \n"
     asm += "xor rax, rax \n"
     asm += "call printf \n"
     return asm
 
+
 def compilExpression(ast):
     if ast.data == "exp_variable":
         return f"mov rax, [{ast.children[0].value}]\n"
-    elif ast.data ==  "exp_nombre":
-        return f"mov rax, {ast.children[0].value}\n"
+    elif ast.data == "exp_nombre":
+        return f"mov rax, {ast.children[0].value.strip()}\n"
     elif ast.data == "exp_binaire":
-        return f"""
-                {compilExpression(ast.children[2])}
+        return f"""{compilExpression(ast.children[2])}
                 push rax
-                {compilExpression(ast.children[0])}
+                {compilExpression(ast.children[0]).strip()}
                 pop rbx
                 {op2asm[ast.children[1].value]}
                 """
--- Parser.py
+++ Parser.py
@@ -1,12 +1,14 @@
 import lark
 
 grammaire = """
-%import common.SIGNED_NUMBER  
+%import common.SIGNED_NUMBER  #bibliothèque lark.
 %import common.WS
 %ignore WS
 // %ignore /[ ]/   #ignore les blancs, mais l'arbre ne contient pas l'information de leur existence. problématique du pretty printer. 
 
 VARIABLE : /[a-zA-Z_][a-zA-Z 0-9]*/
+FUNCTOKEN : /[a-zA-Z_][a-zA-Z 0-9]*/
+STRING : "/[a-zA-Z 0-9]*/"
 NOMBRE : SIGNED_NUMBER
 // NOMBRE : /[1-9][0-9]*/
 OPBINAIRE: /[+*\/&><]/|">="|"-"|">>"  //lark essaie de faire les tokens les plus long possible
@@ -14,33 +16,40 @@ OPBINAIRE: /[+*\/&><]/|">="|"-"|">>"  //lark essaie de faire les tokens les plus
 expression: VARIABLE -> exp_variable
 | NOMBRE         -> exp_nombre
 | expression OPBINAIRE expression -> exp_binaire
+| FUNCTOKEN "(" arguments ")" -> exp_appel //appel de fonction
+| STRING -> exp_string
 
 commande : VARIABLE "=" expression ";"-> com_asgt //les exp entre "" ne sont pas reconnues dans l'arbre syntaxique
 | "printf" "(" expression ")" ";" -> com_printf
 | commande+ -> com_sequence
 | "while" "(" expression ")" "{" commande "}" -> com_while
 | "if" "(" expression ")" "{" commande "}" "else" "{" commande "}" -> com_if
+| "c" VARIABLE "=" STRING ";" -> com_asgt_string
 
 liste_var :                -> liste_vide
 | VARIABLE ("," VARIABLE)* -> liste_normale
-programme : "main" "(" liste_var ")" "{" commande "return" "(" expression ")" ";" "}" -> prog_main // ressemble à une déclaration de fonction
+
+//Function
+arguments :                -> args_vide  # Pas d'arguments.
+    | expression ("," expression)* -> args_normaux  # Arguments de fonction.
+
+fonction : FUNCTOKEN "(" arguments ")" "{" commande* "return" "(" expression ")" ";" "}" -> func
+
+programme : fonction* -> prog
 """
 
-parser = lark.Lark(grammaire, start = "programme")
 
-t = parser.parse("""main(x,y){
-                 while(x) {
-                    y = y + 1;
-                    printf(y);
-                 }
-                 return (y);
-                }
-                 """)
+# main : symbole de départ.
+# VARIABLE : une suite de lettres et de chiffres.
+# main : VARIABLE : la règle de production de main est VARIABLE.
+# expression : VARIABLE | NOMBRE : la règle de production de expression est VARIABLE ou NOMBRE
+#               ou expression '+' expression qui définit une addition.
 
 def pretty_printer_liste_var(t):
-    if t.data == "liste_vide" :
+    if t.data == "liste_vide":
         return ""
     return ", ".join([u.value for u in t.children])
+
 
 def pretty_printer_commande(t):
     if (t.data == "com_asgt"):
@@ -50,15 +59,18 @@ def pretty_printer_commande(t):
     if (t.data == "com_sequence"):
         return "while (%s){ %s}" % (pretty_printer_expression(t.children[0]), pretty_printer_commande(t.children[1]))
     if (t.data == "com_if"):
-        return "if (%s){ %s} else { %s}" % (pretty_printer_expression(t.children[0]), pretty_printer_commande(t.children[1]), pretty_printer_commande(t.children[2]))
+        return "if (%s){ %s} else { %s}" % (
+            pretty_printer_expression(t.children[0]), pretty_printer_commande(t.children[1]),
+            pretty_printer_commande(t.children[2]))
     if (t.data == "com_sequence"):
         return "\n".join([pretty_printer_commande(u) for u in t.children])
-    
+
 
 def pretty_printer_expression(t):
     if t.data in ("exp_variable", "exp_nombre"):
         return t.children[0].value
     return f"{pretty_printer_expression(t.children[0])} {t.children[1].value} {pretty_printer_expression(t.children[2])}"
+
 
 def pretty_printer_commande(t):
     if t.data == "com_asgt":
@@ -68,14 +80,28 @@ def pretty_printer_commande(t):
     if t.data == "com_while":
         return "while (%s){ %s}" % (pretty_printer_expression(t.children[0]), pretty_printer_commande(t.children[1]))
     if t.data == "com_if":
-        return "if (%s){ %s} else { %s}" % (pretty_printer_expression(t.children[0]), pretty_printer_commande(t.children[1]), pretty_printer_commande(t.children[2]))
+        return "if (%s){ %s} else { %s}" % (
+            pretty_printer_expression(t.children[0]), pretty_printer_commande(t.children[1]),
+            pretty_printer_commande(t.children[2]))
     if t.data == "com_sequence":
         return "\n".join([pretty_printer_commande(u) for u in t.children])
 
-def pretty_print(t):
-    return  "main (%s) { %s return (%s); }" % (pretty_printer_liste_var(t.children[0]), 
-                                               pretty_printer_commande(t.children[1]),
-                                                pretty_printer_expression( t.children[2]))
 
-#print(pretty_print(t))
-#print(t)
+def pretty_print(t):
+    return "main (%s) { %s return (%s); }" % (pretty_printer_liste_var(t.children[0]),
+                                              pretty_printer_commande(t.children[1]),
+                                              pretty_printer_expression(t.children[2]))
+
+
+def parse(source_code):
+    parser = lark.Lark(grammaire, start='programme')
+    return parser.parse(source_code)
+
+
+if __name__ == "__main__":
+    parser = lark.Lark(grammaire, start='programme')
+    with open("ideal.c", 'r') as file:
+        tree = parser.parse(file.read())
+    # print(pretty_print(tree))
+    print(tree)
+    print(tree.data)
